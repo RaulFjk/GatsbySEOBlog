992d70b8db8d11263688286d738c3d345aa3a9fd
RaulFjk <tarzanxN13>
2021-05-26 17:22:52 +0300

    siteUrl update

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://www.example.com`,
+    siteUrl: `https://unruffled-allen-27e165.netlify.app`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
