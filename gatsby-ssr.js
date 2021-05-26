8583b4d141248c3b04d3603fa5af3bf89f4f6a04
RaulFjk <tarzanxN13>
2021-05-26 21:30:34 +0300

    static folder google

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,18 +6,3 @@ exports.wrapPageElement = ({element, props}) => {
     return <Layout {...props}>{element}</Layout>
 
 }
-
-exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
-    /**
-     * @type {any[]} headComponents
-     */
-    const headComponents = getHeadComponents();
-
-    headComponents.sort((a, b) => {
-        if (a.props && a.props["data-react-helmet"]) {
-            return 0;
-        }
-        return 1;
-    });
-    replaceHeadComponents(headComponents);
-};
