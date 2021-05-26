d18dbda1dcf42606a3b3ed3311627b93ca5d909c
RaulFjk <tarzanxN13>
2021-05-26 19:22:56 +0300

    import React Helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-import { Helmet } from "react-helmet"
+import  Helmet  from "react-helmet"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import HomePageTemplate from "../templates/homePageTemplate"
